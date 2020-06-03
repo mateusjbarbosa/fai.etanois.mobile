77a6609c639250b03227f486a880d742add20976
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-06-03 17:21:49 -0300

    Limpeza do código

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:etanois/app/app_module.dart';
+
 import 'package:flutter_modular/flutter_modular.dart';
 
 void main() => runApp(ModularApp(module: AppModule()));
