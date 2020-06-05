7ea6de3cb8fc9aab81c5599da6ee029db9af37e1
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-06-04 22:57:48 -0300

    Início da criação da HomePage

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,18 +1,30 @@
-import 'package:etanois/app/app_controller.dart';
-import 'package:etanois/app/modules/chat/chat_module.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'package:etanois/app/modules/chat/chat_module.dart';
+import 'package:etanois/app/modules/home/home_module.dart';
+import 'package:etanois/app/modules/menu/menu_module.dart';
+
+import 'package:etanois/app/modules/home/home_controller.dart';
+import 'package:etanois/app/modules/menu/menu_controller.dart';
+
+import 'package:etanois/app/app_controller.dart';
 import 'package:etanois/app/app_widget.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
+        Bind((i) => MenuController()),
+        Bind((i) => HomeController()),
         Bind((i) => AppController()),
       ];
 
   @override
   List<Router> get routers => [
-        Router(Modular.initialRoute, module: ChatModule()),
+        Router('/', module: ChatModule()),
+        Router('/home', module: HomeModule()),
+        Router('/menu', module: MenuModule()),
       ];
 
   @override
---
+++ lib/app/modules/menu/menu_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'menu_controller.dart';
+
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends ModularState<MenuPage, MenuController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
