2b944f1b238dd2a481bfebe6fb6209c1c22867c5
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-06-06 01:14:33 -0300

    Adição da geolocalização e mostrando a localização do usuário

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,7 +1,84 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobx/mobx.dart';
 
 part 'home_controller.g.dart';
 
 class HomeController = _HomeControllerBase with _$HomeController;
 
-abstract class _HomeControllerBase with Store {}
+abstract class _HomeControllerBase with Store {
+  Completer<GoogleMapController> _controller = Completer();
+
+  CameraPosition userPosition;
+
+  String mapStyle = '';
+
+  void setMapStyle() => rootBundle.loadString('assets/map.style.txt').then((s) => mapStyle = s);
+
+  onMapCreated(GoogleMapController controller) {
+    controller.setMapStyle(mapStyle);
+
+    print(controller);
+
+    _controller.complete(controller);
+  }
+
+  CameraPosition onInitPosition() {
+    CameraPosition position = CameraPosition(
+      target: LatLng(-22.260342, -45.702316),
+      zoom: 16,
+      tilt: 0,
+    );
+
+    userPosition = position;
+
+    return position;
+  }
+
+  moveCamera() async {
+    GoogleMapController camera = await _controller.future;
+
+    camera.animateCamera(
+      CameraUpdate.newCameraPosition(userPosition),
+    );
+  }
+
+  recoverUserLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+
+    CameraPosition newPosition = CameraPosition(
+      target: LatLng(
+        position.latitude,
+        position.longitude,
+      ),
+      zoom: 20,
+      tilt: 0,
+    );
+
+    userPosition = newPosition;
+
+    moveCamera();
+  }
+
+  onListenerUserLocation() async {
+    Geolocator geolocator = Geolocator();
+    LocationOptions locationOptions = LocationOptions(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10,
+      timeInterval: 10,
+    );
+
+    geolocator.getPositionStream(locationOptions).listen((position) {
+      CameraPosition newPosition = CameraPosition(
+        target: LatLng(position.altitude, position.longitude),
+      );
+
+      userPosition = newPosition;
+
+      moveCamera();
+    });
+  }
+}
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,14 +11,51 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
+  Widget _buttonMenu(String route, String icon) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(route);
+      },
+      child: Container(
+        width: 70.0,
+        height: 70.0,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          color: Theme.of(context).accentColor,
+        ),
+        child: Image.asset(
+          icon,
+          height: 35.0,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.setMapStyle();
+
+    controller.onInitPosition();
+    controller.recoverUserLocation();
+    controller.onListenerUserLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-          Container(
-            color: Color(0xFFF7F7F7),
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: controller.userPosition,
+            onMapCreated: controller.onMapCreated,
+            zoomControlsEnabled: false,
+            buildingsEnabled: false,
+            myLocationEnabled: true,
           ),
           Positioned(
             bottom: 30.0,
@@ -25,61 +65,17 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Container(
-                    width: 70.0,
-                    height: 70.0,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      color: Theme.of(context).accentColor,
-                    ),
-                    child: Text(
-                      'R',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                  _buttonMenu(
+                    '/radar',
+                    'assets/icons/radar.png',
                   ),
-                  Container(
-                    width: 70.0,
-                    height: 70.0,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      color: Theme.of(context).accentColor,
-                    ),
-                    child: Text(
-                      'RT',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                  _buttonMenu(
+                    '/route',
+                    'assets/icons/route.png',
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Modular.to.pushNamed('/menu');
-                    },
-                    child: Container(
-                      width: 70.0,
-                      height: 70.0,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                        color: Theme.of(context).accentColor,
-                      ),
-                      child: Text(
-                        'M',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+                  _buttonMenu(
+                    '/menu',
+                    'assets/icons/menu.png',
                   ),
                 ],
               ),
