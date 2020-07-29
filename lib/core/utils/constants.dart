1232610dd4d163715aeef2ce59f6f86c4d85a7b6
Mateus José Barbosa <mateusjbarbosa@MacBook-Air.local>
2020-07-29 17:08:35 -0300

    Início do módulo Core

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,1 @@
+const API_URL_BASE = 'http://api.etanois.com.br/api/v1';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:etanois/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'package:etanois/app/app_module.dart';
 
 void main() => runApp(ModularApp(module: AppModule()));
