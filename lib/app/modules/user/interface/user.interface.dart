2065a73d95b41919a0ef43a4a7fb2dc8b23d58c9
Mateus José Barbosa <mateusjbarbosa@MacBook-Air.local>
2020-07-29 17:30:20 -0300

    Inserção do model, interface e repository de Usuário

---
+++ lib/app/modules/user/interface/user.interface.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:etanois/app/modules/user/model/user.model.dart';
+
+abstract class IUser {
+  Future<Either<dynamic, User>> createUser(User user);
+  Future<Either<dynamic, User>> readUser(int id, String token);
+  Future<Either<dynamic, User>> updateUser(User user);
+  Future<Either<dynamic, User>> deleteUser(int id, String token);
+
+  Future<Either<dynamic, Map<String, dynamic>>> generateUserToken({
+    String email,
+    String password,
+  });
+
+  Future<bool> findUserByEmail(String email);
+  Future<bool> findUserByUsername(String username);
+}
