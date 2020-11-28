ee42a94169167226a6e8a67595939fe84c84cee0
Mateus José Barbosa <mateusjbarbosa@MacBook-Air.local>
2020-11-28 14:52:49 -0300

    Implementação da splash

---
+++ lib/app/modules/splash/splash.page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushNamed(context, '/chat');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              'assets/logotipo.png',
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
