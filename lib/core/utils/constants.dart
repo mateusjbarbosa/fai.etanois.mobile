fae7a9c142ba9ff40aebe5460360f58df5d7ae69
Mateus José Barbosa <mateusjbarbosa@MacBook-Air.local>
2020-12-02 16:56:52 -0300

    Alteração do endereço para a heroku

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,2 +1,3 @@
-const API_URL_BASE = 'http://api.etanois.com.br/api/v1';
+// const API_URL_BASE = 'http://api.etanois.com.br/api/v1';
 // const API_URL_BASE = 'http://52.67.199.188/api/v1';
+const API_URL_BASE = 'https://etanois-backend.herokuapp.com/api/v1';
